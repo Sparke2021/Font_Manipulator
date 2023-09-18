cb2c60531e5946e3386e5153ff9512ad65f94dbf
Sparke2021 <93227213+Sparke2021@users.noreply.github.com>
2023-09-18 18:04:46 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+difference=0;
+rightWristX=0;
+leftWristX=0;
+
 function setup()
 {
     video = createCapture(VIDEO);
@@ -15,16 +19,26 @@ function modelLoaded()
     console.log('PoseNet is Initialized');
 }
 
-function draw()
-{
-    background('#969A97');
-}
-
 function gotPoses(results)
 {
     if(results.length > 0)
     {
         console.log(results);
+                
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX ="+ leftWristX + "rightWristX"+ rightWristX + "difference ="+ difference);
     }
+}
+
+function draw ()
+{
+    background('#6C91C2');
+    textSize(difference);
+    document.getElementById("font_size").innerHTML - "Font size of the text will be - "+ difference +"px";
+    fill ('#FFE787');
+    text ('Salem',50, 400);
 }
     
